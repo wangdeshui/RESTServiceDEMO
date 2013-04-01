7c36ddfe1aa29d09032f4d44e70d9987e92e355f
wangdeshui <wangdeshui@gmail.com>
2013-04-01 10:47:51 +0800

    DEMO

---
+++ RESTServiceDEMO/TESTWCF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TESTWCF.TESTService;
+
+namespace TESTWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TESTService.RestServiceClient client=new RestServiceClient();
+            GetPersonReponse result = client.GetPersons(new GetPresonRequest {PageSize = 10, StartIndex = 0});
+            foreach (var s in result.Persons)
+            {
+                Console.WriteLine(s.Name);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
